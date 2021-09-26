310fc4a5e3529a6123c7bfe12685adab9ab2e346
Diman <diman.suvor.linda.2000@gmail.com>
2021-09-26 08:47:23 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <span>Chess</span>
+
+      <v-spacer></v-spacer>
+      <v-btn depressed>Войти</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <Main/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Main from '@/components/pages/Main'
+
+export default {
+  name: 'App',
+
+  components: {
+    Main,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/pages/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container class="spacing-playground pa-6" fluid>
+    <v-row>
+      <v-col class="d-flex justify-center">
+        <v-btn color="success" large depressed>Начать игру</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
+
+<style scoped>
+
+</style>
